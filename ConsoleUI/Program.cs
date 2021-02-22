5a15614a5e578c506b2084c456f07583a3c9626b
kritize <77644101+kritize@users.noreply.github.com>
2021-02-22 20:58:10 +0300

    22.02.2021 3.commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,33 +5,51 @@ using System;
 
 namespace ConsoleUI
 {
-   
-     //SOLID
-     //Open Closed Principle
-    
+
+    //SOLID
+    //Open Closed Principle
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            //Data Transformation Object
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll()) 
+            foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName);
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+
+                }
 
             }
+            else 
+            {
+                Console.WriteLine(result.Message);
+            }
         }
+
     }
 }
