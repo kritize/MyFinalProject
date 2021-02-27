b51e1e57fe5d67dc7ad1ee04c7f343f45420fd67
kritize <77644101+kritize@users.noreply.github.com>
2021-02-27 21:36:49 +0300

    22.02.2021 3.commit

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -29,6 +29,9 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Autofac,Ninjet,CastleWindsor,structureMap,LightInject,DryInject--> IoC Container
+            //AOP
+            //Postsharp
             services.AddControllers();
             services.AddSingleton<IProductService,ProductManager>();
             services.AddSingleton<IProductDal,EfProductDal>();
